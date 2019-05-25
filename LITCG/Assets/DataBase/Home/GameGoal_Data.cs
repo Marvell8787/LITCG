e7012b719e5079f4b3b63ddb925b8b198ee53e82
Marvell8787 <wendy850122@gmail.com>
2019-05-25 16:10:02 +0800

    20190525
    Interface OK

---
+++ LITCG/Assets/DataBase/Home/GameGoal_Data.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+static class GameGoal_Data{
+
+    public static string[] GameGoal = new string[8] {"1.Complete Task", "2.Complete Learn", "3.Increase Score", "4.Get more Crystal","5.Collect more Cards", "6.Get more Badges", "7.Maintain Points", "8.Less Mistakes" };
+
+}
--- LITCG/Assets/Script/Home/Function_Home.cs
+++ LITCG/Assets/Script/Home/Function_Home.cs
@@ -1,14 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Function_Home : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        ClearAllText();
 
+    }
+    // Show Button
+    public void GameGoals()
+    {
+        Text t_temp;
+        for (int i=0;i<8;i++)
+        {
+            t_temp = GameObject.Find("Text_GameGoal_" + (i + 1).ToString()).GetComponent<Text>();
+            t_temp.text = GameGoal_Data.GameGoal[i];
+        }
+    }
+    public void Status()
+    {
+        //Text t_temp;
+        for (int i = 0; i < 8; i++)
+        {
+            //t_temp = GameObject.Find("Text_GameGoal_" + (i + 1).ToString()).GetComponent<Text>();
+            //t_temp.text = GameGoal_Data.GameGoal[i];
+        }
+    }
+    //GO Button
     public void Task()
     {
         //Application.LoadLevel("Task");
@@ -36,6 +58,29 @@ public class Function_Home : MonoBehaviour {
     public void Introduction()
     {
         Application.LoadLevel("Introduction");
+    }
+    public void ClearAllText()
+    {
+        Text t_temp;
+        for (int i = 0; i < 8; i++)
+        {
+            t_temp = GameObject.Find("Text_GameGoal_" + (i + 1).ToString()).GetComponent<Text>();
+            t_temp.text = "";
+        }
+        t_temp = GameObject.Find("Text_ScoreContent").GetComponent<Text>();
+        t_temp.text = "";
+        t_temp = GameObject.Find("Text_MoneyContent").GetComponent<Text>();
+        t_temp.text = "";
+        t_temp = GameObject.Find("Text_CrystalContent").GetComponent<Text>();
+        t_temp.text = "";
+        t_temp = GameObject.Find("Text_CardsContent").GetComponent<Text>();
+        t_temp.text = "";
+        t_temp = GameObject.Find("Text_BadgesContent").GetComponent<Text>();
+        t_temp.text = "";
+        t_temp = GameObject.Find("Text_PointsContent").GetComponent<Text>();
+        t_temp.text = "";
+        t_temp = GameObject.Find("Text_MistakesContent").GetComponent<Text>();
+        t_temp.text = "";
     }
 
 }
