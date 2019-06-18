12cd328ab470e519f2e8f8a03889cf55588bb26c
Marvell8787 <wendy850122@gmail.com>
2019-06-18 22:23:25 +0800

    20190618

--- LITCG/Assets/DataBase/Card/Card_Data.cs
+++ LITCG/Assets/DataBase/Card/Card_Data.cs
@@ -7,9 +7,9 @@ static class Card_Data{
     private static string[] Card_CType = new string[22];
     private static string[] Card_Picture = new string[22] { "fool", "magician","high-priestess", "empress","emperor", "hierophant", "hermit" , "chariot","death","lovers", "temperance", "hanged-man", "justice" ,"judgement","sun","moon","stars","strength", "fortune-wheel", "tower", "devil", "world" };
     private static string[] Card_Name = new string[22] { "The Fool","The Magician" , "The High PRIESTESS","The EMPRESS","The EMPEROR","The HIEROPHANT", "THE HERMIT", "THE CHARIOT" ,"THE DEATH","THE LOVERS","THE TEMPERANCE", "THE HANGED MAN", "THE JUSTICE" ,"THE JUDGEMENT","The SUN","THE MOON","THE START","STRENGTH", "WHEEL OF FORTUNE","THE TOWER", "THE DEVIL", "THE WORLD" };
-    private static string[] Card_Rarity = new string[22];
+    private static string[] Card_Rarity = new string[22] { "N","N","N","N","N","N","N","R", "R", "R","SR", "SR", "SSR", "SSR" ,"R", "R", "R", "SR", "SR", "SR", "SR", "SSR" };
     private static string[] Card_Description = new string[22];
-    private static int[] Card_ATK = new int[22];
+    private static int[] Card_ATK = new int[22] { 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4, 4, 5,0,0,0,0,0,0,0,0,0 };
     private static string[] Card_Effect = new string[22];
 
     private static Card_Class[] card_temp = new Card_Class[22];
@@ -24,6 +24,18 @@ static class Card_Data{
                 Card_CType[i] = "Magic";
             else if (i >18)
                 Card_CType[i] = "Support";
+        }
+        for (int i = 0; i < 22; i++)
+        {
+            switch (i)
+            {
+                case 14: Card_Effect[i] = "Player LP + 3"; break;
+                case 15: Card_Effect[i] = "Destory 1 Fight Card"; break;
+                case 16: Card_Effect[i] = "Enemy LP - 3"; break;
+                case 17: Card_Effect[i] = "Draw 2 Card"; break;
+
+            }
+
         }
 
         //宣告 level_temp 陣列並加入資料 Start
