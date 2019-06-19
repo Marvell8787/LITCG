9a12d67d4304ba1f4d5278650f4e498592736547
Marvell8787 <46806099+Marvell8787@users.noreply.github.com>
2019-06-20 01:06:01 +0800

    20190620

---
+++ LITCG/Assets/Script/Guide/Function_Guide.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Function_Guide : MonoBehaviour {
+
+	public void Task () {
+        Text T_Temp;
+        T_Temp = GameObject.Find("Text_Content").GetComponent<Text>();
+        T_Temp.text = "";
+	}
+    public void Learn(){
+        Text T_Temp;
+        T_Temp = GameObject.Find("Text_Content").GetComponent<Text>();
+        T_Temp.text = "";
+    }
+    public void Battle()
+    {
+
+    }
+    public void Status()
+    {
+
+    }
+
+}
