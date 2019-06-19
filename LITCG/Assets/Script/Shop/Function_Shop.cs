a5f1850cf8390b7d5fab38634fcf0e523e8386ff
Marvell8787 <wendy850122@gmail.com>
2019-06-19 21:46:36 +0800

    20190619

--- LITCG/Assets/Script/Shop/Function_Shop.cs
+++ LITCG/Assets/Script/Shop/Function_Shop.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Function_Shop : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public void Card1()
+    {
+        Button B_Temp;
+
+        if(Learner_Data.Learner_GetData("Coin") >= 100)
+        {
+            B_Temp = GameObject.Find("Button_Buy_1").GetComponent<Button>();
+            B_Temp.interactable = false;
+            Learner_Data.Learner_Add("Coin", -100);
+        }
+        
+    }
+
     public void Back()
     {
         Application.LoadLevel("Home");
