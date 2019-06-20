b8852f9bb39b5cee61db099a34754b4f4094ab9c
Marvell8787 <wendy850122@gmail.com>
2019-06-20 08:08:15 +0800

    20190620
    Profile Badge Leaderboard
    Interface Over

--- LITCG/Assets/Script/Badges/Function_Badges.cs
+++ LITCG/Assets/Script/Badges/Function_Badges.cs
@@ -4,10 +4,7 @@ using UnityEngine;
 
 public class Function_Badges : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	
     public void Back()
     {
         Application.LoadLevel("Profile");
---
+++ LITCG/Assets/Script/Badges/Image_Badges.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Image_Badges : MonoBehaviour {
+    public void Badges_Output(int n)
+    {
+        //Image I_Temp;
+        Text T_temp;
+        //I_Temp = GameObject.Find("Image_Show").GetComponent<Image>();
+        //I_Temp.sprite = Resources.Load("Image/Card/" + card_temp.GetPicture(), typeof(Sprite)) as Sprite;
+
+        T_temp = GameObject.Find("Text_Description").GetComponent<Text>();
+        T_temp.text = n.ToString();
+    }
+    public void Badges_0()
+    {
+        Badges_Output(0);
+    }
+    public void Badges_1()
+    {
+        Badges_Output(1);
+    }
+    public void Badges_2()
+    {
+        Badges_Output(2);
+    }
+    public void Badges_3()
+    {
+        Badges_Output(3);
+    }
+    public void Badges_4()
+    {
+        Badges_Output(4);
+    }
+    public void Badges_5()
+    {
+        Badges_Output(5);
+    }
+    public void Badges_6()
+    {
+        Badges_Output(6);
+    }
+    public void Badges_7()
+    {
+        Badges_Output(7);
+    }
+    public void Badges_8()
+    {
+        Badges_Output(8);
+    }
+
+}
