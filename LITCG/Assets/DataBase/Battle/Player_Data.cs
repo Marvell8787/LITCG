dbf85a3aca22331435e25110586818afab167e84
Marvell8787 <46806099+Marvell8787@users.noreply.github.com>
2019-08-03 23:25:39 +0800

    20190803
    Roomfight

--- LITCG/Assets/DataBase/Battle/Player_Data.cs
+++ LITCG/Assets/DataBase/Battle/Player_Data.cs
@@ -4,30 +4,17 @@ using UnityEngine;
 
 static class Player_Data{
 
-    private static Player_Class[] Player = new Player_Class[2];  // 0:自己 1:敵人
+    private static Player_Class Player ;  // 0:自己 1:敵人
 
     public static void Player_Init()
     {
         //Debug.Log(Learner_Data.Learner_GetData("Cards_Num").ToString());
-        Player[0] = new Player_Class(20, Learner_Data.Learner_GetData("Cards_Num"), 5, 0); //Card_Num = 10
-        Player[1] = new Player_Class(20,22,5,0);
-
-        for(int i = 0; i < 22; i++)
-        {
-
-        }
-
-        for(int i = 0; i < 22; i++)
-        {
-            Player[1].ChangeDeck_Status(i,i+1);
-        }
-
+        Player = new Player_Class(20, Learner_Data.Learner_GetData("Cards_Num"), 5, 0); //Card_Num = 10
     }
 
     public static Player_Class Player_Get(int n)
     {
-        return Player[n];
-        
+        return Player;
     }
     public static void Update_Deck()
     {
--- LITCG/Assets/Script/RoomFight/Canvas_RoomFight.cs
+++ LITCG/Assets/Script/RoomFight/Canvas_RoomFight.cs
@@ -6,23 +6,18 @@ using UnityEngine.UI;
 
 public class Canvas_RoomFight : MonoBehaviour {
 
-    private Player_Class[] Player = new Player_Class[2];  // 0:自己 1:敵人
+    private Player_Class Player = new Player_Class();  // 0:自己 1:敵人
     private bool f = false;
     // Use this for initialization
     void Start () {
 
-        Player[0] = Player_Data.Player_Get(0);
-        Player[1] = Player_Data.Player_Get(1);
+        Player = Player_Data.Player_Get(0);
 
         Text t_temp;
         t_temp = GameObject.Find("Text_LP_A_num").GetComponent<Text>();
-        t_temp.text = Player[0].GetLP().ToString();
-        t_temp = GameObject.Find("Text_LP_B_num").GetComponent<Text>();
-        t_temp.text = Player[1].GetLP().ToString();
+        t_temp.text = Player.GetLP().ToString();
         t_temp = GameObject.Find("Text_Deck_A_num").GetComponent<Text>();
-        t_temp.text = (Player[0].GetDeck_Num()-5).ToString();
-        t_temp = GameObject.Find("Text_Deck_B_num").GetComponent<Text>();
-        t_temp.text = (Player[1].GetDeck_Num()-5).ToString();
+        t_temp.text = (Player.GetDeck_Num()-5).ToString();
 
 
     }
