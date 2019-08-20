3f611a1cac18f03c1aab3bfb41d6624728c4b0f5
Marvell8787 <46806099+Marvell8787@users.noreply.github.com>
2019-08-20 13:13:11 +0800

    20190820
    RoomWait

---
+++ LITCG/Assets/Script/RoomWait/Canvas_RoonWait.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Canvas_RoonWait : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Text t_temp;
+        t_temp = GameObject.Find("Text_A_LP_Num").GetComponent<Text>();
+        t_temp.text = "20";
+        t_temp = GameObject.Find("Text_A_Deck_Num").GetComponent<Text>();
+        t_temp.text = Learner_Data.Learner_GetData("Cards_Num").ToString();
+        switch (Enemy.No)
+        {
+            case 1:
+                t_temp = GameObject.Find("Text_B_LP_Num").GetComponent<Text>();
+                t_temp.text = "10";
+                t_temp = GameObject.Find("Text_B_Deck_Num").GetComponent<Text>();
+                t_temp.text = "14";
+                t_temp = GameObject.Find("Text_Time_Num").GetComponent<Text>();
+                t_temp.text = "10";
+                t_temp = GameObject.Find("Text_Range_Num").GetComponent<Text>();
+                t_temp.text = "1 - 10";
+                break;
+            case 2:
+                t_temp = GameObject.Find("Text_B_LP_Num").GetComponent<Text>();
+                t_temp.text = "15";
+                t_temp = GameObject.Find("Text_B_Deck_Num").GetComponent<Text>();
+                t_temp.text = "17";
+                t_temp = GameObject.Find("Text_Time_Num").GetComponent<Text>();
+                t_temp.text = "15";
+                t_temp = GameObject.Find("Text_Range_Num").GetComponent<Text>();
+                t_temp.text = "1 - 15";
+                break;
+            case 3:
+                t_temp = GameObject.Find("Text_B_LP_Num").GetComponent<Text>();
+                t_temp.text = "20";
+                t_temp = GameObject.Find("Text_B_Deck_Num").GetComponent<Text>();
+                t_temp.text = "20";
+                t_temp = GameObject.Find("Text_Time_Num").GetComponent<Text>();
+                t_temp.text = "20";
+                t_temp = GameObject.Find("Text_Range_Num").GetComponent<Text>();
+                t_temp.text = "1 - 20";
+                break;
+            default:
+                break;
+        }
+    }
+}
